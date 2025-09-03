024c8277c00599b03c6c01c9553838f7b0aaae56
Farzan Haider Kazmi <158690438+FarzanHaider@users.noreply.github.com>
2025-09-03 12:33:20 +0500

    Delete backend directory

--- backend/Routes/contactRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { sendEmail } from '../Controllers/contactController.js';
-
-const router = express.Router();
-
-// Route for sending email
-router.post('/send-email', sendEmail);
-
-export default router;
